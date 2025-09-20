6229c4b2036b9ea5af1d83c934f10cc26ce5ef3f
arpat1 <mvr0636@gmail.com>
2025-09-20 22:40:45 +0400

    Get new variables from config in python

--- Bueelder.py
+++ Bueelder.py
@@ -14,6 +14,8 @@ load_dotenv(dotenv_path="bueeld.config.env")
 config = {
     "PATH_TO_DEV_HTML": os.getenv("PATH_TO_DEV_HTML"),
     "PATH_TO_PROD_HTML": os.getenv("PATH_TO_PROD_HTML"),
+    "PATH_TO_APP": os.getenv("PATH_TO_APP"),
+    "PATH_TO_APP_ENV": os.getenv("PATH_TO_APP_ENV"),
     "PORT": int(os.getenv("PORT"))
 }
 
