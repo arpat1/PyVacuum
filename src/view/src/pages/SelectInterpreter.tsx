b923988f257443d5ccbcd5b0b5d0eb7cbcd3da70
arpat1 <mvr0636@gmail.com>
2025-10-03 16:07:26 +0400

    Make selecting buttons layout

--- src/view/src/pages/SelectInterpreter.tsx
+++ src/view/src/pages/SelectInterpreter.tsx
@@ -1,8 +1,31 @@
+import React from "react"
+import styles from "./SelectInterpreter.module.css"
+
 const SelectInterpreter = () => {
+    const createRipples = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        const el = e.currentTarget
+        const ripples = document.createElement("span")
+        let posx = e.clientX - e.currentTarget.offsetLeft
+        let posy = e.clientY - e.currentTarget.offsetTop
+
+        ripples.style.left = posx + "px"
+        ripples.style.top = posy + "px"
+
+        el.appendChild(ripples)
+
+        setTimeout(() => {
+            ripples.remove()
+        }, 1000)
+    }
     return (
-        <>
-            <p>Select Interpreter Page</p>
-        </>
+        <div className={styles.container}>
+            <a href="#" className={styles["select-ripple-btn"]} onClick={createRipples}>
+                Select files to run
+            </a>
+            <a href="#" className={styles["select-ripple-btn"]} onClick={createRipples}>
+                Select interpreter
+            </a>
+        </div>
     )
 }
 
